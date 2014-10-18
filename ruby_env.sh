c08147b249a790ee702182d5baba2b47490dac56
Kyle Suss <susskyle@gmail.com>
2014-10-18 12:50:28 -0600

    removed conditional logic in redis

--- ruby_env.sh
+++ ruby_env.sh
@@ -50,11 +50,11 @@ then
   exit 1
 else
   echo "Installing ruby..."
-  # curl $URL/modules/ruby.sh | bash
+  curl $URL/modules/ruby.sh | bash
 fi
 
 if [ "$REDIS" = true ]
 then
   echo "Installing redis..."
-  # curl $REDIS/modules/redis.sh | bash
+  curl $REDIS/modules/redis.sh | bash
 fi
