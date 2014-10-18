b063c1f8bec70a09402f147b7a5f97e2611a89f4
Kyle Suss <susskyle@gmail.com>
2014-10-18 12:46:47 -0600

    lots of conditional logic for flags and params

--- ruby_env.sh
+++ ruby_env.sh
@@ -1,10 +1,61 @@
 #!/usr/bin/env bash
 
+URL=""
+
+#///////////////////////
+#// Check Args Exist
+#///////////////////////
+
 if [ $# -eq 0 ]
 then
-  echo "A valid URL with the -s flag in order to dynamically load modules. See https://github.com/kylesuss/ubuntu-bootstrap for details."
+  echo "A valid URL is required in order to dynamically load modules. See https://github.com/kylesuss/ubuntu-bootstrap for details."
   exit 1
 fi
 
-curl $1/modules/ruby.sh | bash
-curl $1/modules/redis.sh | bash
+#///////////////////////
+#// Set Variables for Args
+#///////////////////////
+
+while
+  (( $# > 0 ))
+do
+  key="$1"
+  shift
+  case $key in
+
+    (-url)
+      URL="$1"
+      shift
+    ;;
+
+    (--redis)
+      REDIS=true
+      shift
+    ;;
+
+    (*)
+      echo "Unknown argument passed. See https://github.com/kylesuss/ubuntu-bootstrap for valid arguments."
+      exit 1
+    ;;
+
+  esac
+done
+
+#///////////////////////
+#// Conditional Loading
+#///////////////////////
+
+if [ "$URL" = "" ]
+then
+  echo "A valid URL is required in order to dynamically load modules. See https://github.com/kylesuss/ubuntu-bootstrap for details."
+  exit 1
+else
+  echo "Installing ruby..."
+  curl $URL/modules/ruby.sh | bash
+fi
+
+if [ "$REDIS" = true ]
+then
+  echo "Installing redis..."
+  curl $REDIS/modules/redis.sh | bash
+fi
