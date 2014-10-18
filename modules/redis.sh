7b35e7f122e33c9acc893d5f5a6de3c3f6035326
Kyle Suss <susskyle@gmail.com>
2014-10-18 13:04:28 -0600

    added yes assumption for apt-get in redis

--- modules/redis.sh
+++ modules/redis.sh
@@ -5,8 +5,8 @@ echo "Installing redis..."
 #// Dependencies
 #///////////////////////
 
-sudo apt-get install build-essential
-sudo apt-get install tcl8.5
+sudo apt-get --assume-yes install build-essential
+sudo apt-get --assume-yes install tcl8.5
 
 #///////////////////////
 #// Install
