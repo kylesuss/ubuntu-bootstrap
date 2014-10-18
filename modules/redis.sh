40f2288328b4604062c6fda1608fe8721fa756f9
Kyle Suss <susskyle@gmail.com>
2014-10-18 14:56:46 -0600

    changed wget to curl

--- modules/redis.sh
+++ modules/redis.sh
@@ -7,12 +7,13 @@ echo "Installing redis..."
 
 sudo apt-get --assume-yes install build-essential
 sudo apt-get --assume-yes install tcl8.5
+sudo apt-get --assume-yes autoremove
 
 #///////////////////////
 #// Install
 #///////////////////////
 
-wget http://download.redis.io/releases/redis-2.8.17.tar.gz
+curl http://download.redis.io/releases/redis-2.8.17.tar.gz
 tar xzf redis-2.8.17.tar.gz
 cd redis-2.8.17
 make
