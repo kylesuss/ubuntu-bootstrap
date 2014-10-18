13afcca9c9017cf2c87ef857ca686b61a53699f7
Kyle Suss <susskyle@gmail.com>
2014-10-18 14:40:36 -0600

    variable assignment

--- ruby_env.sh
+++ ruby_env.sh
@@ -3,16 +3,6 @@
 URL=""
 DEFAULT_URL="https://raw.githubusercontent.com/kylesuss/ubuntu-bootstrap/master"
 
-# #///////////////////////
-# #// Check Args Exist
-# #///////////////////////
-
-# if [ $# -eq 0 ]
-# then
-#   echo "A valid URL is required in order to dynamically load modules. See https://github.com/kylesuss/ubuntu-bootstrap for details."
-#   exit 1
-# fi
-
 #///////////////////////
 #// Set Variables for Args
 #///////////////////////
@@ -43,7 +33,7 @@ done
 
 if [ "$URL" = "" ]
 then
-  URL=$DEFAULT_URL
+  URL="$DEFAULT_URL"
   curl $URL/modules/ruby.sh | bash
 else
   curl $URL/modules/ruby.sh | bash
