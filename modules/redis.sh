f9ecbfc91e3126521bafc9ae8d91733a0a912639
Kyle Suss <susskyle@gmail.com>
2014-10-18 21:31:26 -0600

    added advanced config for redis

--- modules/redis.sh
+++ modules/redis.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 echo "Installing redis..."
 
@@ -5,15 +6,24 @@ echo "Installing redis..."
 #// Install
 #///////////////////////
 
-wget --retry-connrefused --waitretry=10 http://download.redis.io/releases/redis-2.8.17.tar.gz | bash
-tar xzf redis-2.8.17.tar.gz
-cd redis-2.8.17
+wget --retry-connrefused --waitretry=10 http://download.redis.io/redis-stable.tar.gz | bash
+tar xzf redis-stable.tar.gz
+cd redis-stable
 make
 sudo make install
-cd utils
 
 #///////////////////////
-#// Startup
+#// Config
 #///////////////////////
+
+sudo mkdir /etc/redis
+sudo mkdir /var/redis
+sudo cp utils/redis_init_script /etc/init.d/redis_6379
+sudo cp redis.conf /etc/redis/6379.conf
+sudo mkdir /var/redis/6379
+sed  -i.bak -e 's/daemonize no.*/daemonize yes/' /etc/redis/6379.conf
+sed  -i.bak -e 's/pidfile \/var\/run\/redis\.pid.*/pidfile \/var\/run\/redis\_6379\.pid/' /etc/redis/6379.conf
+sed  -i.bak -e 's/logfile \"\".*/logfile \/var\/log\/redis\_6379\.log/' /etc/redis/6379.conf
+sed  -i.bak -e 's/dir \.\/.*/dir \/var\/redis\/6379/' /etc/redis/6379.conf
 
 sudo update-rc.d redis_6379 defaults
