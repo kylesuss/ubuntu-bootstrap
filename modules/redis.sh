e1cf8f96ef72a473c620efb3b804e6c210804a51
Kyle Suss <susskyle@gmail.com>
2014-10-18 21:43:21 -0600

    added -p flag on redis mkdirs

--- modules/redis.sh
+++ modules/redis.sh
@@ -16,14 +16,14 @@ sudo make install
 #// Config
 #///////////////////////
 
-sudo mkdir /etc/redis
-sudo mkdir /var/redis
+sudo mkdir -p /etc/redis
+sudo mkdir -p /var/redis
 sudo cp utils/redis_init_script /etc/init.d/redis_6379
 sudo cp redis.conf /etc/redis/6379.conf
-sudo mkdir /var/redis/6379
-sed  -i.bak -e 's/daemonize no.*/daemonize yes/' /etc/redis/6379.conf
-sed  -i.bak -e 's/pidfile \/var\/run\/redis\.pid.*/pidfile \/var\/run\/redis\_6379\.pid/' /etc/redis/6379.conf
-sed  -i.bak -e 's/logfile \"\".*/logfile \/var\/log\/redis\_6379\.log/' /etc/redis/6379.conf
-sed  -i.bak -e 's/dir \.\/.*/dir \/var\/redis\/6379/' /etc/redis/6379.conf
+sudo mkdir -p /var/redis/6379
+sed -i.bak -e 's/daemonize no.*/daemonize yes/' /etc/redis/6379.conf
+sed -i.bak -e 's/pidfile \/var\/run\/redis\.pid.*/pidfile \/var\/run\/redis\_6379\.pid/' /etc/redis/6379.conf
+sed -i.bak -e 's/logfile \"\".*/logfile \/var\/log\/redis\_6379\.log/' /etc/redis/6379.conf
+sed -i.bak -e 's/dir \.\/.*/dir \/var\/redis\/6379/' /etc/redis/6379.conf
 sudo update-rc.d redis_6379 defaults
 /etc/init.d/redis_6379 start
