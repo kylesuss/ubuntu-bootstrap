666d71823c6b07b7bbc4ca447f843df29ce2500a
Kyle Suss <susskyle@gmail.com>
2014-10-20 13:42:43 -0600

    added logic for rvm root install

--- modules/ruby.sh
+++ modules/ruby.sh
@@ -8,9 +8,18 @@ echo "Installing ruby..."
 
 \curl -sSL https://get.rvm.io | bash -s stable
 
-# Assuming an install path of /usr/local/rvm/
-echo '[[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"' >> ~/.bashrc
-source /usr/local/rvm/scripts/rvm
+# Reload the shell
+. ./.bash_profile && . ./.profile && . ./.bashrc
+
+if [ `whoami` = 'root' ]
+then
+  # Assuming an install path of /usr/local/rvm/
+  # Have noticed that RVM isn't automatically appending paths to shell profiles as root
+  echo '[[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"' >> ~/.bashrc
+  source /usr/local/rvm/scripts/rvm  
+else
+  source "$HOME/.rvm/scripts/rvm"
+fi
 
 # Get latest ruby
 rvm install ruby --latest
